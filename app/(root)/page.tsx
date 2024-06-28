31934d1b705d9ce2c06c2a80792331b2729e8f27
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-06-28 12:45:53 +0300

    chore - layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
         </header>
         RECENT TRANSACTIONS
       </div>
-      <RightSidebar user={loggedIn} transactions={[]} banks={[]}/>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50}, {currentBalance: 123.50}]}/>
     </section>
   )
 }
