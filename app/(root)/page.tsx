b2f10b7fcf8d4b897cd14a0b32381245c8364dcd
Moldovan Dennis <dennismoldovan32@gmail.com>
2024-06-28 12:00:13 +0300

    chore - sidebars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
+import HeaderBox from "@/components/HeaderBox"
+import RightSidebar from "@/components/RightSidebar"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = () => {
+
+  const loggedIn = { firstName: "Dennis", lastName: "Moldovan", email: "contact@jsmastery.pro"}
+
   return (
-    <div>
-      
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || "Guest"} subtext="Access and manage your account and transactions efficiently." />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]}/>
+    </section>
   )
 }
 
